5e6990231f964932019188b89be77ade7b4143b9
Mark <36781160+mobious999@users.noreply.github.com>
2021-12-20 21:01:10 -0500

    initial checkin

--- multiaccount/application/accounts/dev/ecr/main.tf
+++ multiaccount/application/accounts/dev/ecr/main.tf
@@ -1,87 +1,83 @@
-resource "aws_ecr_repository" "repo" {
-  name                 = var.name
-  image_tag_mutability = var.image_tag_mutability
+module "ecr" {
 
-  # Encryption configuration
-  dynamic "encryption_configuration" {
-    for_each = local.encryption_configuration
-    content {
-      encryption_type = lookup(encryption_configuration.value, "encryption_type")
-      kms_key         = lookup(encryption_configuration.value, "kms_key")
-    }
-  }
+  source = "lgallard/ecr/aws"
 
-  # Image scanning configuration
-  dynamic "image_scanning_configuration" {
-    for_each = local.image_scanning_configuration
-    content {
-      scan_on_push = lookup(image_scanning_configuration.value, "scan_on_push")
-    }
-  }
+  name                 = "ecr-repo-dev"
+  scan_on_push         = true
+  timeouts_delete      = "60m"
+  image_tag_mutability = "MUTABLE"
 
-  # Timeouts
-  dynamic "timeouts" {
-    for_each = local.timeouts
-    content {
-      delete = lookup(timeouts.value, "delete")
-    }
-  }
+  # Note that currently only one policy may be applied to a repository.
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "repo policy",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
+
+  # Only one lifecycle policy can be used per repository.
+  # To apply multiple rules, combined them in one policy JSON.
+  lifecycle_policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire untagged images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep last 30 dev images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["dev"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+
 
   # Tags
-  tags = var.tags
+  tags = {
+    Owner       = "DevOps team"
+    Environment = "dev"
+    Terraform   = true
+  }
 
-}
-
-# Policy
-resource "aws_ecr_repository_policy" "policy" {
-  count      = var.policy == null ? 0 : 1
-  repository = aws_ecr_repository.repo.name
-  policy     = var.policy
-}
-
-# Lifecycle policy
-resource "aws_ecr_lifecycle_policy" "lifecycle_policy" {
-  count      = var.lifecycle_policy == null ? 0 : 1
-  repository = aws_ecr_repository.repo.name
-  policy     = var.lifecycle_policy
-}
-
-# KMS key
-resource "aws_kms_key" "kms_key" {
-  count       = var.encryption_type == "KMS" && var.kms_key == null ? 1 : 0
-  description = "${var.name} KMS key"
-}
-
-resource "aws_kms_alias" "kms_key_alias" {
-  count         = var.encryption_type == "KMS" && var.kms_key == null ? 1 : 0
-  name          = "alias/${var.name}Key"
-  target_key_id = aws_kms_key.kms_key[0].key_id
-}
-
-locals {
-
-  # Encryption configuration
-  # If encryption type as KMS, use assigned KMS key or otherwise build a new key
-  encryption_configuration = var.encryption_type != "KMS" ? [] : [
-    {
-      encryption_type = "KMS"
-      kms_key         = var.encryption_type == "KMS" && var.kms_key == null ? aws_kms_key.kms_key[0].arn : var.kms_key
-    }
-  ]
-
-  # Image scanning configuration
-  # If no image_scanning_configuration block is provided, build one using the default values
-  image_scanning_configuration = [
-    {
-      scan_on_push = lookup(var.image_scanning_configuration, "scan_on_push", null) == null ? var.scan_on_push : lookup(var.image_scanning_configuration, "scan_on_push")
-    }
-  ]
-
-  # Timeouts
-  # If no timeouts block is provided, build one using the default values
-  timeouts = var.timeouts_delete == null && length(var.timeouts) == 0 ? [] : [
-    {
-      delete = lookup(var.timeouts, "delete", null) == null ? var.timeouts_delete : lookup(var.timeouts, "delete")
-    }
-  ]
 }
